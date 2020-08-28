131f0119a00cb608869c9e110d1db45a9c094cdf
Thanakorn Nitithumbundit <t.nitithumbundit@gmail.com>
2020-08-28 11:04:32 +1000

    Revert "Initial commit"
    This reverts commit 3d0da3187742f4662b5929e489ec0b25c040d7c5.

--- sudoku/check_version.py
+++
@@ -1,8 +0,0 @@
-# check version
-import os
-import sys
-print(os.path.dirname(sys.executable))
-# C:\\ProgramData\\Anaconda3
-
-import numpy as np
-print(np.array([1,2]))
--- tests/untitled.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import unittest
-# from sudoku import sudoku_solver
-import os
-
-print(os.getcwd())
